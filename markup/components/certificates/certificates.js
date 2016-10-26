6a99cc2a1542f2750e8900c4d5e9a212894f43d9
extiser <1sappho@gmail.com>
2016-10-26 16:05:15 +0500

    #del удаление все, кроме public_html

--- markup/components/certificates/certificates.js
+++
@@ -1,8 +0,0 @@
-$('.js-certificates').slick({
-  infinite: true,
-  arrows: false,
-  dots: true,
-  adaptiveHeight: true,
-  autoplay: true,
-  autoplaySpeed: 3000
-});
--- markup/components/factory/factory.js
+++
@@ -1,8 +0,0 @@
-$('.js-showToggle-btn').click(function (e) {
-  e.preventDefault();
-  var toggleText = $(this).data('toggle');
-  $(this).closest('.js-showToggle').find('.js-showToggle-text').slideToggle();
-  
-  $(this).data('toggle', $(this).html());
-  $(this).html(toggleText);
-})
